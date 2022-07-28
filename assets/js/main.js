c59d27c5ea6c87cc5e2d058568dfce8eab32a8cb
unknown <ajibolamubarak304@gmail.com>
2022-07-28 04:13:43 +0100

    JS menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+/*======== MENU==========*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 /*=============== CHANGE BACKGROUND HEADER ===============*/
