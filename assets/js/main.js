2a14b09725bbc6c1e2d17ac4c994e2033386aeb1
unknown <ajibolamubarak304@gmail.com>
2022-07-29 02:53:25 +0100

    JS update

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,21 @@ function changeSize() {
 
 sizes.forEach((size) => size.addEventListener("click", changeSize));
 
+function changeColor() {
+  let primary = this.getAttribute("primary");
+  let color = this.getAttribute("color");
+  let sneakerColor = document.querySelector(`.sneaker__img[color="${color}"]`);
+
+  colors.forEach((c) => c.classList.remove("active"));
+  this.classList.add("active");
+
+  document.documentElement.style.setProperty("--primary", primary);
+
+  sneaker.forEach((s) => s.classList.remove("shows"));
+  sneakerColor.classList.add("shows");
+}
+
+colors.forEach((c) => c.addEventListener("click", changeColor));
 // sizes.forEach((el) => {
 //   el.addEventListener("click", (e) => {
 //     e.currentTarget.classList.add("active");
